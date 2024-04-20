ea9704cb0f3af15e869d8aafe811343eb22e8ef5
Joseph Ofori Amanfo <jayyy@Josephs-MBP.lan>
2024-04-20 11:06:09 -0400

    initial commit

---
+++ src/test/java/com/jayyy/krom/KromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayyy.krom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
